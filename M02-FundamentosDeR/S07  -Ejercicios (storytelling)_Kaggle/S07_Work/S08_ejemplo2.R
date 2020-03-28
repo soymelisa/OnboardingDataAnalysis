e91d8b4dd803e52b9c909f32766397c46b1168ee
Melisa Perez <soymelisa@yahoo.com>
2020-03-28 13:22:11 -0600

    ejemplos y reto regresión lineal

--- M02-FundamentosDeR/S07  -Ejercicios (storytelling)_Kaggle/S07_Work/S08_ejemplo2.R
+++ M02-FundamentosDeR/S07  -Ejercicios (storytelling)_Kaggle/S07_Work/S08_ejemplo2.R
@@ -1,5 +1,5 @@
 head(iris)
-random <- sample(1:nrow(iris), 0.89 * nrow(iris)) 
+random <- sample(1:nrow(iris), 0.9 * nrow(iris)) 
 normalizado <-function(x) {(x -min(x))/(max(x)-min(x))}
 iris_normalizado <- as.data.frame(lapply(iris[,c(1,2,3,4)], normalizado))
 iris_training <- iris_normalizado[random,] 
@@ -11,3 +11,6 @@ predic <- knn(iris_training,iris_test,cl=iris_categoria_target,k=13)
 con_mat <- table(predic,iris_categoria_test)
 precision <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
 precision(con_mat)
+
+
+
