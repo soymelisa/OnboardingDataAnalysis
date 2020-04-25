39c161e33dd0b53ffe85db326fdf98e8bddc82f0
Melisa Pérez <soymelisa@users.noreply.github.com>
2020-04-25 14:20:16 -0500

    imprimir tabla de array de manera simple

--- M03-FundamentosPython-DA/S02/reporte.py
+++ M03-FundamentosPython-DA/S02/reporte.py
@@ -1,23 +1,30 @@
 # Generando estructuras de datos
 
-recorrido1 = {
-    "ORIGEN":"REFORMA", 
-    "DESTINO": "Tabacalera", 
-    "DISTANCIA": 3.5,
-    "TIEMPO": "15:00"
-}
+recorridos = [
+    {
+        "ORIGEN":"Reforma", 
+        "DESTINO": "Tabacalera", 
+        "DISTANCIA": 3.5,
+        "TIEMPO": "15:00"
+    },
+    {
+        "ORIGEN":"Reforma", 
+        "DESTINO": "Juárez", 
+        "DISTANCIA": 1.2,
+        "TIEMPO": "08:00"
+    },
+    {
+        "ORIGEN":"Alameda", 
+        "DESTINO": "Condesa", 
+        "DISTANCIA": 5.4,
+        "TIEMPO": "20:00"
+    }
 
-recorrido2 = {
-    "ORIGEN":"Reforma", 
-    "DESTINO": "Juárez", 
-    "DISTANCIA": 1.2,
-    "TIEMPO": "08:00"
-}
+]
 
+#Imprimimos tabla (vista)
+print("-"*40)
+for recorrido in recorridos:
+    print("{ORIGEN:10} {DESTINO:10} {DISTANCIA:10} {TIEMPO:10}".format(**recorrido))
+print ("-"*43)
 
-recorrido3 = {
-    "ORIGEN":"REFORMA", 
-    "DESTINO": "Tabacalera", 
-    "DISTANCIA": 3.5,
-    "TIEMPO": "15:00"
-}
